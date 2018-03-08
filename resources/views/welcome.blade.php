74cb3c38cc8059dc1b1efd0a99173f33db8e2725
vagrant <vagrant@homestead>
2018-03-08 21:51:41 +0000

    usersZone first changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,15 +83,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to our team
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/">Сoming soon...</a>
+                    <a href="/">Сoming soon...</a>
+                    <a href="/">Сoming soon...</a>
+                    <a href="/">Сoming soon...</a>
+                    <a href="https://github.com/so-technologies">Our GitHub</a>
                 </div>
             </div>
         </div>
